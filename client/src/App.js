32a70ae2884ded707a132b323e3dddef7bfa2301
Maya Tripathy <maya.tripathy1@gmail.com>
2023-02-21 13:46:12 +0100

    added react router and set up basic components for home, funds, and budget pages with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import HomeView from "./components/HomeView.js";
+import Budget from "./components/Budget.js";
+import Funds from "./components/Funds.js";
 import './App.css';
 
 function App() {
@@ -6,7 +10,15 @@ function App() {
 
   return (
     <div className="App">
-      Hello, welcome to React!
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/budget">Budget</Link> | <Link to="/funds">Funds</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/budget" element={<Budget />} />
+        <Route path="/funds" element={<Funds />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/Budget.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Budget() {
+  return (
+    <div>Budget</div>
+  )
+}
