8a0df3128ad036a7d1186a6dfc401c9e745a3e8e
Maya Tripathy <maya.tripathy1@gmail.com>
2023-02-21 17:49:21 +0100

    wrote get by id for costs

--- routes/costs.js
+++ routes/costs.js
@@ -27,4 +27,18 @@ router.post('/', async function(req, res, next) {
   }
 })
 
+router.get('/:id', async function (req, res, next) {
+  let id = Number(req.params.id);
+  try {
+    let result = await db(`SELECT * FROM cost_actual WHERE id = ${id}`);
+    if (result.data.length === 0) {
+      res.status(404).send({error: 'item not found'})
+    } else {
+      res.send(result.data)
+    }
+  } catch(err) {
+    res.status(500).send({error: err.message})
+  }
+})
+
 module.exports = router;
