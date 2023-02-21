7a01f94f51ed3d7bbb26a8665b5d9628f10f2309
Maya Tripathy <maya.tripathy1@gmail.com>
2023-02-21 18:04:10 +0100

    wrote delete methods for each route

--- routes/costs.js
+++ routes/costs.js
@@ -12,6 +12,20 @@ router.get('/', async function(req, res, next) {
   }
 });
 
+router.get('/:id', async function (req, res, next) {
+  let id = Number(req.params.id);
+  try {
+    let result = await db(`SELECT * FROM cost_actual WHERE id = ${id}`);
+    if (result.data.length === 0) {
+      res.status(404).send({error: 'item not found'})
+    } else {
+      res.send(result.data)
+    }
+  } catch(err) {
+    res.status(500).send({error: err.message})
+  }
+});
+
 router.post('/', async function(req, res, next) {
   let newCostItem = req.body;
   let sql = `
@@ -27,13 +41,15 @@ router.post('/', async function(req, res, next) {
   }
 })
 
-router.get('/:id', async function (req, res, next) {
-  let id = Number(req.params.id);
+router.delete('/:id', async function(req, res, next) {
+  let id = req.params.id;
   try {
     let result = await db(`SELECT * FROM cost_actual WHERE id = ${id}`);
-    if (result.data.length === 0) {
-      res.status(404).send({error: 'item not found'})
+    if (result.data.length === 0){
+      res.status(404).send({error: 'item not found'});
     } else {
+      await db(`DELETE FROM cost_actual WHERE id = ${id}`);
+      let result = await db(`SELECT * FROM cost_actual`);
       res.send(result.data)
     }
   } catch(err) {
--- routes/income.js
+++ routes/income.js
@@ -27,4 +27,20 @@ router.post('/', async function(req, res, next) {
   }
 })
 
+router.delete('/:id', async function(req, res, next){
+  let id = req.params.id;
+  try {
+    let result = await db(`SELECT * FROM income WHERE id = ${id}`);
+    if (result.data.length === 0){
+      res.status(404).send({error: 'item not found'})
+    } else {
+      await db(`DELETE FROM income WHERE id = ${id}`);
+      let result = await db(`SELECT * FROM income`);
+      res.send(result.data)
+    }
+  } catch(err) {
+    res.status(500).send({error: err.message})
+  }
+})
+
 module.exports = router;
