7c7c1917febe658d0fea61cf0cbc78ef51a0b3ad
Maya Tripathy <maya.tripathy1@gmail.com>
2023-02-22 14:55:41 +0100

    added home logo into nav bar

--- client/src/components/Budget.js
+++ client/src/components/Budget.js
@@ -3,14 +3,13 @@ import Funds from "./Funds.js";
 import HomeView from "./HomeView.js";
 import { Route, Routes, Link } from "react-router-dom";
 import "./Budget.css";
+import logo from "../images/logo.png";
 
 export default function Budget() {
   return (
     <div className='Budget'>
       <nav>
-        <div>
-          <Link to="/">Home</Link>
-        </div>
+        <Link to="/"  id="logo"><img src={logo} alt="Home"></img></Link>
         <div id="nav-right">
           <Link to="/budget" className="nav-link">Budget</Link>
           <Link to="/funds" className="nav-link">Funds</Link>
--- client/src/components/Funds.js
+++ client/src/components/Funds.js
@@ -3,14 +3,13 @@ import HomeView from "./HomeView.js";
 import Budget from "./Budget.js";
 import { Route, Routes, Link } from "react-router-dom";
 import "./Funds.css";
+import logo from "../images/logo.png";
 
 export default function Funds() {
   return (
     <div className="Funds">
       <nav>
-        <div>
-          <Link to="/">Home</Link>
-        </div>
+      <Link to="/"  id="logo"><img src={logo} alt="Home"></img></Link>
         <div id="nav-right">
           <Link to="/budget" className="nav-link">Budget</Link>
           <Link to="/funds" className="nav-link">Funds</Link>
