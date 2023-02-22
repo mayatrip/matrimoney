5930639d88b2b43b3c1fb245da5a6c936b9b57a3
Maya Tripathy <maya.tripathy1@gmail.com>
2023-02-22 12:17:40 +0100

    added buttons for home page and styled

--- client/src/components/HomeView.js
+++ client/src/components/HomeView.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import "./HomeView.css";
+import Budget from "./Budget.js";
+import Funds from "./Funds.js";
+import { Route, Routes, Link } from "react-router-dom";
 
 export default function HomeView() {
   return (
     <div className="HomeView">
       <h1>MATRIMONEY</h1>
+      <div id="homepage-buttons">
+        <Link to="/budget">
+          <button type="button">VIEW MY BUDGET</button>
+        </Link>
+        <Link to="/funds">
+          <button type="button">ADD FUNDS</button>
+        </Link>
+      </div>
+
+      <Routes>
+        <Route path="/budget" element={<Budget />} />
+        <Route path="/funds" element={<Funds />} />
+      </Routes>
     </div>
   )
 }
