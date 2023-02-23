0ea4eebe13e4e8e7b4c3ce532d265b57f169731f
Maya Tripathy <maya.tripathy1@gmail.com>
2023-02-23 11:20:19 +0100

    moved display of estimated costs into its own component so budget can hold the css and display info shared by estimated, actual, and compare

--- client/src/components/Budget.js
+++ client/src/components/Budget.js
@@ -1,29 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Funds from "./Funds.js";
 import HomeView from "./HomeView.js";
 import { Route, Routes, Link, Outlet } from "react-router-dom";
 import "./Budget.css";
 import logo from "../images/logo.png";
-import ActualCost from './ActualCost.js';
-import Compare from './Compare.js';
-import EstimatedCostForm from './EstimatedCostForm.js';
-import MatrimoneyApi from '../MatrimoneyApi.js';
+import EstimatedCost from './EstimatedCost.js';
 
 export default function Budget() {
-  const [estimatedCosts, setEstCosts] = useState([]);
 
-  useEffect(() => {
-    getCostEstimate();
-  }, []);
-
-  async function getCostEstimate(){
-    let uresponse = await MatrimoneyApi.getCostEstimate();
-    if (uresponse.ok){
-      setEstCosts(uresponse.data);
-    } else {
-      console.log(`Error! ${uresponse.error}`)
-    }
-  }
 
   return (
     <div className='Budget'>
@@ -35,75 +19,7 @@ export default function Budget() {
         </div>
       </nav>
       <h2>My Budget</h2>
-      <div className="secondary-nav">
-        <Link to="/budget" className="selected-second-nav">Estimated</Link>
-        <Link to="/budget/costs" >Actual</Link>
-        <Link to="/budget/compare" >Compare</Link>
-      </div>
-      <div id="bottom-container">
-        <div id="table-div">
-        <div id="table-div-left">
-          <h3>
-            Our Recommendations:*
-          </h3>
-          <table>
-            <tbody>
-              <tr>
-                <th>Vendor</th>
-                <th>Cost</th>
-              </tr>
-              <tr>
-                <td>Venue</td>
-                <td>$</td>
-              </tr>
-              <tr>
-                <td>Venue</td>
-                <td>$</td>
-              </tr>
-              <tr>
-                <td>Venue</td>
-                <td>$</td>
-              </tr>
-              <tr>
-                <td>Venue</td>
-                <td>$</td>
-              </tr>
-              <tr>
-                <td>Venue</td>
-                <td>$</td>
-              </tr>
-              <tr>
-                <td>Venue</td>
-                <td>$</td>
-              </tr>
-            </tbody>
-          </table>
-          <p>
-            *Recommendation based off of your budget and average breakdown of wedding costs
-          </p>
-        </div>
-        <div id="table-div-right">
-          <h3>
-            Estimated Costs:
-          </h3>
-          <table>
-            <tbody>
-              <tr>
-                <th>Vendor</th>
-                <th>Cost</th>
-              </tr>
-              {estimatedCosts.map(c => (
-                <tr key={c.id}>
-                  <td>{c.text}</td>
-                  <td>${c.amount}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <EstimatedCostForm />
-        </div>
-        </div>
-      </div>
+      <EstimatedCost />
     </div>
   )
 }
---
+++ client/src/components/EstimatedCost.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react';
+import MatrimoneyApi from '../MatrimoneyApi.js';
+import ActualCost from './ActualCost.js';
+import Compare from './Compare.js';
+import EstimatedCostForm from './EstimatedCostForm';
+import { Route, Routes, Link, Outlet } from "react-router-dom";
+
+export default function EstimatedCostDisplay() {
+  const [estimatedCosts, setEstCosts] = useState([]);
+
+  useEffect(() => {
+    getCostEstimate();
+  }, []);
+
+  async function getCostEstimate(){
+    let uresponse = await MatrimoneyApi.getCostEstimate();
+    if (uresponse.ok){
+      setEstCosts(uresponse.data);
+    } else {
+      console.log(`Error! ${uresponse.error}`)
+    }
+  }
+
+  return (
+    <div>
+    <div className="secondary-nav">
+        <Link to="/budget" className="selected-second-nav">Estimated</Link>
+        <Link to="/budget/costs" >Actual</Link>
+        <Link to="/budget/compare" >Compare</Link>
+      </div>
+      <div id="bottom-container">
+        <div id="table-div">
+        <div id="table-div-left">
+          <h3>
+            Our Recommendations:*
+          </h3>
+          <table>
+            <tbody>
+              <tr>
+                <th>Vendor</th>
+                <th>Cost</th>
+              </tr>
+              <tr>
+                <td>Venue</td>
+                <td>$</td>
+              </tr>
+              <tr>
+                <td>Venue</td>
+                <td>$</td>
+              </tr>
+              <tr>
+                <td>Venue</td>
+                <td>$</td>
+              </tr>
+              <tr>
+                <td>Venue</td>
+                <td>$</td>
+              </tr>
+              <tr>
+                <td>Venue</td>
+                <td>$</td>
+              </tr>
+              <tr>
+                <td>Venue</td>
+                <td>$</td>
+              </tr>
+            </tbody>
+          </table>
+          <p>
+            *Recommendation based off of your budget and average breakdown of wedding costs
+          </p>
+        </div>
+        <div id="table-div-right">
+          <h3>
+            Estimated Costs:
+          </h3>
+          <table>
+            <tbody>
+              <tr>
+                <th>Vendor</th>
+                <th>Cost</th>
+              </tr>
+              {estimatedCosts.map(c => (
+                <tr key={c.id}>
+                  <td>{c.text}</td>
+                  <td>${c.amount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <EstimatedCostForm />
+        </div>
+        </div>
+      </div>
+    </div>
+  )
+}
